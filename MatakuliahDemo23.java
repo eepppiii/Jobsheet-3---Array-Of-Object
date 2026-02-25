35e5458ca7dbcc2956de37da5432f56873b2cb4a
reffi igamaiyoda <email_gitbuh_igmaiyoda10@gmail.com>
2026-02-25 10:53:03 +0700

    Update

--- MatakuliahDemo23.java
+++ MatakuliahDemo23.java
@@ -3,34 +3,34 @@ import java.util.Scanner;
 public class MatakuliahDemo23 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Matakuliah23[] arrayofMatakuliah23 = new Matakuliah23[3];
+        System.out.print("Masukkan jumlah matakuliah: ");
+        int jumlah = Integer.parseInt(sc.nextLine());
+        Matakuliah23[] arrayofMatakuliah23 = new Matakuliah23[jumlah];
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for (int i = 0; i < 3; i++) {
-            System.out.println("Masukkan Data Matakuliah ke- " +(i+1));
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("Masukkan Data Matakuliah ke- " + (i + 1));
             System.out.print("Kode :");
-            kode  = sc.nextLine();
+            kode = sc.nextLine();
             System.out.print("Nama :");
-            nama  = sc.nextLine();
+            nama = sc.nextLine();
             System.out.print("SKS :");
-            dummy  = sc.nextLine();
+            dummy = sc.nextLine();
             sks = Integer.parseInt(dummy);
             System.out.print("Jumlah Jam :");
             dummy = sc.nextLine();
             jumlahJam = Integer.parseInt(dummy);
             System.out.println("================");
 
-            arrayofMatakuliah23[i] = new Matakuliah23(kode, nama, sks, jumlahJam);
+            arrayofMatakuliah23[i] = new Matakuliah23();
+            arrayofMatakuliah23[i].tambahData(kode, nama, sks, jumlahJam);
         }
 
-        for(int i=0; i<3 ; i++){
-            System.out.println("Data Matakuliah ke-" +(i+1));
-            System.out.println("Kode   :" + arrayofMatakuliah23[i].kode);
-            System.out.println("Nama  :" + arrayofMatakuliah23[i].nama);
-            System.out.println("SKS   :" + arrayofMatakuliah23[i].sks);
-            System.out.println("JumlahJam   :" + arrayofMatakuliah23[i].jumlahJam);
-            System.out.println("-----------------------------------");
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("Data Matakuliah ke-" + (i + 1));
+            arrayofMatakuliah23[i].cetakInfo();
         }
+
     }
 }
